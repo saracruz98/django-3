f0b5776b355a4dd282ea975e8e6eb857e10b249f
saris <sariscruz98@gmail.com>
2026-06-19 17:11:41 -0500

    chore(settings): switch to SQLite for development

--- inventario_django/dcrm/dcrm/settings.py
+++ inventario_django/dcrm/dcrm/settings.py
@@ -74,13 +74,9 @@ WSGI_APPLICATION = 'dcrm.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dcrm_db',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
